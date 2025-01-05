2680a24e655e8097a091693336cc944b85d325e3
petrvytisk <osvytisk@gmail.com>
2025-01-05 15:55:48 +0100

    Initial commit
    Základní struktura projektu. Vytvořen první model - Order

---
+++ FinalSchoolProject/ApplicationDbContext.cs
@@ -0,0 +1,9 @@
+﻿using FinalSchoolProject.Models;
+using Microsoft.EntityFrameworkCore;
+
+internal class ApplicationDbContext :DbContext {
+    public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options) : base(options) {
+
+    }
+    public DbSet<Order> Orders { get; set; }
+}
---
+++ FinalSchoolProject/Models/Order.cs
@@ -0,0 +1,13 @@
+﻿using System.ComponentModel.DataAnnotations.Schema; //je tohle potřeba?, j to v magistrech
+
+namespace FinalSchoolProject.Models {
+    public class Order {
+        public int Id { get; set; }
+        public DateOnly Accepted { get; set; }
+        public DateOnly Deadline { get; set; }
+        public string Description { get; set; }
+        public double TotalPrice { get; set; }
+        public int DaysLeft { get; set; }
+
+    }
+}
