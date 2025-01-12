09c23698833833ea82e5872c99af87210f4e3796
petrvytisk <osvytisk@gmail.com>
2025-01-12 19:11:27 +0100

    Bootstrap upgrade
    Odstranění BS v 5.1. Nainstalována v 5.3.3 přes NPM. Dále Gulp pro auto kompilaci scss souboru. Došlo k úpravám v hierarchii souborů a k prolinkování nových potřebných souborů.

---
+++ FinalSchoolProject/gulpfile.js
@@ -0,0 +1,24 @@
+﻿const gulp = require('gulp');
+const sass = require('gulp-sass')(require('sass'));
+const path = require('path');
+
+// Cesty k SCSS a CSS souborům
+const paths = {
+    scss: './wwwroot/scss/**/*.scss', // Zdrojové SCSS soubory
+    css: './wwwroot/css'             // Výstupní CSS složka
+};
+
+// Task pro kompilaci SCSS na CSS
+gulp.task('sass', function () {
+    return gulp.src(paths.scss)         // Zdrojové SCSS soubory
+        .pipe(sass().on('error', sass.logError)) // Kompilace SCSS
+        .pipe(gulp.dest(paths.css));    // Výstupní CSS složka
+});
+
+// Watch task pro sledování změn v SCSS souborech
+gulp.task('watch', function () {
+    gulp.watch(paths.scss, gulp.series('sass'));
+});
+
+// Defaultní task
+gulp.task('default', gulp.series('sass', 'watch'));
