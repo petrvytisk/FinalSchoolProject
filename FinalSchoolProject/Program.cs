674658418cafd206ea36e6bc0279cf65a542a3fb
petrvytisk <osvytisk@gmail.com>
2025-04-08 11:37:06 +0200

    Finální verze pro odevzdání projektu

--- FinalSchoolProject/Program.cs
+++ FinalSchoolProject/Program.cs
@@ -8,8 +8,11 @@ var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 builder.Services.AddControllersWithViews();
+//builder.Services.AddDbContext<ApplicationDbContext>(options => {
+//    options.UseSqlServer(builder.Configuration["ConnectionStrings:OMSAppDbConnection"]);
+//});
 builder.Services.AddDbContext<ApplicationDbContext>(options => {
-    options.UseSqlServer(builder.Configuration["ConnectionStrings:OMSAppDbConnection"]);
+    options.UseSqlServer(builder.Configuration["ConnectionStrings:MonsterAspDbConnection"]);
 });
 builder.Services.AddIdentity<AppUser, IdentityRole>().AddEntityFrameworkStores<ApplicationDbContext>().AddDefaultTokenProviders();
 builder.Services.AddScoped<OrderService>();
@@ -50,6 +53,7 @@ if (!app.Environment.IsDevelopment())
     app.UseExceptionHandler("/Home/Error");
     // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
     app.UseHsts();
+    app.UseDeveloperExceptionPage();
 }
 
 app.UseHttpsRedirection();
