fda85f961e49b0968e993ad5b7748d62fb96ebe6
petrvytisk <osvytisk@gmail.com>
2025-07-18 14:10:43 +0200

    Deployed version 1.1.0 - Oprava chyby při zadávání hesla, změny na front-endu

--- FinalSchoolProject/Program.cs
+++ FinalSchoolProject/Program.cs
@@ -8,6 +8,7 @@ var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 builder.Services.AddControllersWithViews();
+// ________________________ TOTO ZAKOMENTOVAT VE VERZI CO PUJDE NA WEB ________________________________________ - je to local db
 //builder.Services.AddDbContext<ApplicationDbContext>(options => {
 //    options.UseSqlServer(builder.Configuration["ConnectionStrings:OMSAppDbConnection"]);
 //});
